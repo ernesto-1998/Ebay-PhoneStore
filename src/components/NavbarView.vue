f760b4deb4df038cc3b178c338cb93bbd2863f0a
ernesto-1998 <ernestoalonso1998@gmail.com>
2022-11-12 00:01:33 -0600

    bootstrap implementado

--- src/components/NavbarView.vue
+++ src/components/NavbarView.vue
@@ -46,16 +46,75 @@
         <SideNav @change-state="change" />
       </div> -->
     </nav>
-    <nav class="movil-container visibilidad2">
+    <!-- <nav class="movil-container visibilidad2">
       <div class="burguer-icon">
         <i class="bx bx-menu bx-lg"></i>
       </div>
-      <ul class="hide-menu">
-        <li class="hide-menu_item">1</li>
-        <li class="hide-menu_item">2</li>
-        <li class="hide-menu_item">3</li>
-        <li class="hide-menu_item">4</li>
-      </ul>
+    </nav> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light visibilidad2">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><hr class="dropdown-divider" /></li>
+                <li>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link disabled"
+                href="#"
+                tabindex="-1"
+                aria-disabled="true"
+                >Disabled</a
+              >
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input
+              class="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button class="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
     </nav>
   </header>
 </template>
@@ -109,19 +168,6 @@ const searchEmit = () => {
 
 <style scoped>
 /* Estilos para navbar movil */
-
-.movil-container {
-  position: fixed;
-  width: 100%;
-}
-
-.hide-menu {
-  position: absolute;
-  width: 100%;
-  top: -100px;
-  background-color: var(--main-color);
-}
-
 .visibilidad2 {
   display: none;
 }
